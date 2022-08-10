baa5d379255f3707af60b425c091e6b77d9b3029
pGarpOYsx41o3OKx_PTL <273606826@qq.com>
2022-08-10 23:26:30 +0800

    完成 添加学生功能
    在添加学生的时候，加入了循环判断输入数据是否合法。其中性别是二值取值，所以逻辑判断符选择“||”，而年龄，成绩这些是在一个区间上取值，所以应该使用"&&"，我一开始使用“||”，导致了无法正常进行非法数据的判断。

--- 学生管理系统/学生管理系统.c
+++ 学生管理系统/学生管理系统.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // CONSTANT DEFINITION
@@ -7,6 +8,31 @@
 
 // define the maximum number of student is 1000
 #define MAX 1000
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// STRUCTION DEFINITION
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// encapsulate the structure of student
+// students' information include name, gender, age, C score, linear Algebra score and calculus score
+typedef struct student
+{
+    char m_Name[10];              /* student name */
+    int m_Gender;                 /* student gender */
+    int m_Age;                    /* student age */
+    float m_C_Score;              /* C score */
+    float m_Linear_Algebra_Score; /* linear Algebra score */
+    float m_Calculus_Score;       /* calculus score */
+} stu;
+
+// encapsulate the structure of system
+typedef struct management_System
+{
+    // student array
+    struct student student_Array[MAX];
+    // student number at present
+    int m_Student_Number;
+} manager;
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // FUNCTION DEFINITION
@@ -27,34 +53,113 @@ void showMenu(void)
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 }
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// STRUCTION DEFINITION
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-// encapsulate the structure of student
-// students' information include name, gender, age, C score, linear Algebra score and calculus score
-struct student
+// 1.Add Student
+void add_Student(manager *core)
 {
-    char m_Name[10];              /* student name */
-    int m_Gender;                 /* student gender */
-    int m_Age;                    /* student age */
-    float m_C_Score;              /* C score */
-    float m_Linear_Algebra_Score; /* linear Algebra score */
-    float m_Calculus_Score;       /* calculus score */
-};
+    // check whether the system is full
+    if (core->m_Student_Number == MAX)
+    {
+        printf("This mangement system is full, you cannot add any student!\n");
+    }
+    else
+    {
+        // add student name
+        char name[30];
+        printf("Please input student's name: ");
+        scanf("%s", name);
+        strcpy(core->student_Array[core->m_Student_Number].m_Name, name);
 
-// encapsulate the structure of system
-struct management_System
-{
-    // student array 
-    struct student student_Array[MAX];
-    // student number at present
-    int m_Student_Number;
-};
+        // add student gender: 1 -> man, 0 -> woman
+        printf("Please input stdeunt's gender: ");
+        int gender = 0;
+        while (1)
+        {
+            // if enter 1 or 0, you can break the loop
+            // else the gender is worry, you will try again
+            scanf("%d", &gender);
+            if (gender == 1 || gender == 0)
+            {
+                core->student_Array[core->m_Student_Number].m_Gender = gender;
+                break;
+            }
+            printf("The gender you input is worry, please input again: ");
+        }
+
+        // add student age
+        printf("Please input student's age: ");
+        int age = 0;
+        while (1)
+        {
+            // if the age is in (0, 100), you can break the loop
+            // else the age is worry, you will try again
+            scanf("%d", &gender);
+            if (gender > 0 && gender < 100)
+            {
+                core->student_Array[core->m_Student_Number].m_Age = age;
+                break;
+            }
+            printf("The age you input is worry, please input again: ");
+        }
+
+        // add student C score
+        printf("Please input student's C program lecture score: ");
+        float c_Score = 0;
+        while (1)
+        {
+            // if the score is in [0, 100], you can break the loop
+            // else the score is worry, you will try again
+            scanf("%f", &c_Score);
+            if (c_Score >= 0 && c_Score <= 100)
+            {
+                core->student_Array[core->m_Student_Number].m_C_Score = c_Score;
+                break;
+            }
+            printf("The C program lecture score you input is worry, please input again: ");
+        }
+
+        // add student Linear Algebra score
+        printf("Please input student's Linear Algebra lecture score: ");
+        float linear_Algebra_Score = 0;
+        while (1)
+        {
+            // if the score is in [0, 100], you can break the loop
+            // else the score is worry, you will try again
+            scanf("%f", &linear_Algebra_Score);
+            if (linear_Algebra_Score >= 0 && linear_Algebra_Score <= 100)
+            {
+                core->student_Array[core->m_Student_Number].m_Linear_Algebra_Score = linear_Algebra_Score;
+                break;
+            }
+            printf("The Linear Algebra lecture score you input is worry, please input again: ");
+        }
+
+        // add student calculus score
+        printf("Please input student's Calculus lecture score: ");
+        float calculus_Score = 0;
+        while (1)
+        {
+            // if the score is in [0, 100], you can break the loop
+            // else the score is worry, you will try again
+            scanf("%f", &calculus_Score);
+            if (calculus_Score >= 0 && calculus_Score <= 100)
+            {
+                core->student_Array[core->m_Student_Number].m_Linear_Algebra_Score = calculus_Score;
+                break;
+            }
+            printf("The Calculus lecture score you input is worry, please input again: ");
+        }
+    }
+}
 
 int main(void)
 {
     int select = 0;
+
+    // create a management system structure
+    manager core;
+
+    // initialize the current number of student in this core
+    core.m_Student_Number = 0;
 
     // encapsulate the loop to finish the selection
     while (1)
@@ -68,7 +173,8 @@ int main(void)
         switch (select)
         {
         case 1:
-            // 1.Add Student
+            // use address transfer
+            add_Student(&core);
             break;
         case 2:
             // 2.Display Student
