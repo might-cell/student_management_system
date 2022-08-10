495874bf77f4df4605961b8ad2494e8f0d4f2a91
pGarpOYsx41o3OKx_PTL <273606826@qq.com>
2022-08-10 21:58:18 +0800

    完成显示菜单的功能

--- 学生管理系统/学生管理系统.c
+++ 学生管理系统/学生管理系统.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// FUNCTION DECLARATION
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// encapsulate the function to display the operation interface
+void showMenu(void)
+{
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("-=-=-=-=-=-=-=   1.Add Student       -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   2.Display Student   -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   3.Delete Student    -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   4.Search Student    -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   5.Modify Student    -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   6.Wipe Student      -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=   0.Exit System       -=-=-=-=-=-=-=\n");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+}
+
+
 int main(void)
 {
-
+    // invoke the function to show the operation interface
+    showMenu();
     return 0;
 }
